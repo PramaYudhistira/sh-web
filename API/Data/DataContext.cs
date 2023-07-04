19b0f37e5b61bf437c9e3ba1d3ad6a041820f61b
unknown <pyudhistira02@gmail.com>
2023-07-04 19:38:45 -0400

    connection string

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using API.Entities;
 
 namespace API.Data
 {
     public class DataContext : DbContext
     {
+        //passing in connection string as an option 
         public DataContext(DbContextOptions options) : base(options)
         {
-            
+
         }
+
+
+        //creates a table for Users
+        //Columns: UserId, UserName
+        public DbSet<AppUser> Users {get; set;}
     }
 }
