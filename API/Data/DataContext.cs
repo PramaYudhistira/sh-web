eb07dff01ea5ffa1e0c070568495b6e9b9a3e27d
unknown <pyudhistira02@gmail.com>
2023-07-04 19:57:31 -0400

    more connection string setup

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -9,15 +9,21 @@ namespace API.Data
 {
     public class DataContext : DbContext
     {
-        //passing in connection string as an option 
+        /// <summary>
+        /// constructor for DataContext that calls the DbContext constructor that passes in the connection string
+        /// </summary>
+        /// <param name="options">the connection string being passed in</param>
+        /// <returns></returns>
         public DataContext(DbContextOptions options) : base(options)
         {
 
         }
 
-
         //creates a table for Users
         //Columns: UserId, UserName
+        /// <summary>
+        /// DbSet for AppUser entity, will correspond to a table with columns UserId, UserName
+        /// </summary>
         public DbSet<AppUser> Users {get; set;}
     }
 }
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,13 @@ namespace API.Entities
 {
     public class AppUser
     {
+
+
+        /// <summary>
+        /// Primary key for the AppUser table, use this for RELATIONSHIP ESTABLISHMENT
+        /// </summary>
+        /// <value>The integer representing UserId</value>
+        [Key]
         public int UserId { get; set; }
 
         public string UserName { get; set; }
